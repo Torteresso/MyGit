ca9acbe3705dc12c6a4d466c69c042417d62ba3d
Wilfried RAFFI <wilfried.raffi@gmail.com>
2026-04-07 17:45:18 +0200

    test: remove dummy false test

--- app/src/test/kotlin/MyGitTest.kt
+++ app/src/test/kotlin/MyGitTest.kt
@@ -1,24 +1,8 @@
-import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class MyGitTest {
     @Test
     fun alwaysTrue() {
 
-    }
-
-    @Test
-    fun alwaysFalse() {
-        assertEquals(2, 1)
-    }
-
-    @Test
-    fun alwaysTrue2() {
-        assertEquals(2, 2)
-    }
-
-    @Test
-    fun alwaysFalse2() {
-        assertEquals(2, 3)
     }
 }
