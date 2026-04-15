a000a4cdccba2afbfdb0670a4cda24517cbebc5d
Wilfried RAFFI <wilfried.raffi@gmail.com>
2026-04-15 12:22:14 +0200

    refactor: add an android module

---
+++ android-app/build.gradle.kts
@@ -0,0 +1,71 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.compose.compiler)
+}
+
+android {
+    namespace = "com.example.android_app"
+    compileSdk {
+        version = release(36) {
+            minorApiLevel = 1
+        }
+    }
+
+    defaultConfig {
+        applicationId = "com.example.android_app"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    buildFeatures {
+        compose = true
+    }
+
+}
+
+dependencies {
+    val composeBom = platform("androidx.compose:compose-bom:2026.03.00")
+    implementation(composeBom)
+    androidTestImplementation(composeBom)
+
+    // Choose one of the following:
+    // Material Design 3
+    implementation(libs.material3)
+    // or skip Material Design and build directly on top of foundational components
+    implementation(libs.foundation)
+    // or only import the main APIs for the underlying toolkit systems,
+    // such as input and measurement/layout
+    implementation(libs.ui)
+    // Android Studio Preview support
+    implementation(libs.ui.tooling.preview)
+    debugImplementation(libs.ui.tooling)
+    // UI Tests
+    androidTestImplementation(libs.ui.test.junit4)
+    debugImplementation(libs.ui.test.manifest)
+    // Optional - Integration with activities
+    implementation(libs.activity.compose)
+    // Optional - Integration with ViewModels
+    implementation(libs.lifecycle.viewmodel.compose)
+    // Optional - Integration with LiveData
+    implementation(libs.runtime.livedata)
+    // GitLogic
+    implementation(project(":git-logic"))
+
+}
--- git-logic/build.gradle.kts
+++ git-logic/build.gradle.kts
@@ -1,9 +1,7 @@
 plugins {
     kotlin("jvm")
 }
-repositories {
-    mavenCentral()
-}
+
 dependencies{
     // For INI-configuration
     implementation("org.apache.commons:commons-configuration2:2.13.0")
