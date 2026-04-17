0b04e0146df41b017d3453e9db8da5d08e53884a
Wilfried RAFFI <wilfried.raffi@gmail.com>
2026-04-17 17:01:51 +0200

    build: update JGit and Apache-common versions

--- git-logic/build.gradle.kts
+++ git-logic/build.gradle.kts
@@ -2,10 +2,10 @@ plugins {
     kotlin("jvm")
 }
 
-dependencies{
+dependencies {
     // For INI-configuration
-    implementation("org.apache.commons:commons-configuration2:2.13.0")
+    implementation(libs.commons.configuration2)
 
     //JGit
-    implementation("org.eclipse.jgit:org.eclipse.jgit:7.6.0.202603022253-r")
+    implementation(libs.org.eclipse.jgit)
 }
