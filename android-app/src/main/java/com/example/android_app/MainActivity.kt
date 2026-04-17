942ce03f172a826f7a9f8ca5d46cec1a760f5de5
Wilfried RAFFI <wilfried.raffi@gmail.com>
2026-04-17 11:31:59 +0200

    feat: basic android activity skeleton

--- android-app/src/main/java/com/example/android_app/MainActivity.kt
+++ android-app/src/main/java/com/example/android_app/MainActivity.kt
@@ -1,47 +1,16 @@
 package com.example.android_app
 
 import android.os.Bundle
-import android.os.storage.StorageManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.core.content.getSystemService
-import java.util.UUID
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MaterialTheme(
-
-            ) {
-                val storageManager = applicationContext.getSystemService<StorageManager>()!!
-                val appSpecificInternalDirUuid: UUID = storageManager.getUuidForPath(filesDir)
-                val availableBytes: Long =
-                    storageManager.getAllocatableBytes(appSpecificInternalDirUuid)
-
-                Surface(
-                    modifier = Modifier.fillMaxSize()
-                ) {
-                    Row(
-                        horizontalArrangement = Arrangement.Center,
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Text(
-                            text = applicationContext.filesDir.toString()
-                        )
-                    }
-                }
-            }
+            GitPuzzles()
         }
     }
 }
