5b439d2eda302182ce87aa023afe06a589fbba2e
Wilfried RAFFI <wilfried.raffi@gmail.com>
2026-04-17 13:33:11 +0200

    feat: first init button

--- android-app/src/main/java/com/example/android_app/ui/HomeScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.example.android_app.ui
-
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-
-@Composable
-fun HomeScreen() {
-    Text("Hello world")
-}
